b610c0cf80f723d7985e282c879ab27974d1b46d
nofarkotler <49288542+nofarkotler@users.noreply.github.com>
2020-04-01 11:43:32 +0300

    Add files via upload

---
+++ Flask_Form.py
@@ -0,0 +1,36 @@
+import paramiko
+from flask import Flask, render_template, request  # Import the Flask class
+
+app = Flask(__name__)  # create an instance of this class
+
+@app.route("/")  # tell Flask what URL should trigger our function
+def Html_Form():
+    return render_template('html1.htm')  # run the Html Form
+
+# the paramiko connection
+@app.route("/", methods=['POST'])
+def Remote_connect():
+    x = request.form['name1']
+    y = request.form['name2']
+    print("please wait, creating ssh client...")
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # avoid the assurance question before connecting
+    print("please wait, connecting with remote server... ")
+    ssh_client.connect(hostname='192.168.8.103', username='root', password='berly123!')  # uses port 22
+    cmd = "cd /root/Flask; python script.py;"
+    print("please wait executing command on remote server...")
+    stdin, stdout, stderr = ssh_client.exec_command(cmd)
+    stdin.write(x + '\n')
+    stdin.write(y + '\n')
+    stdin.flush()
+    print("Succesfully executed command on remote server")
+    stdout = stdout.readlines()
+    stdout = "".join(stdout)  # more readable
+    return render_template('handler.htm', text=stdout)
+    ssh_client.close()
+
+
+if __name__ == "__main__":  # This will run the application.This will help us trace the errors.
+    app.run(debug=True)
+
+
---
+++ Remote_Script.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+
+x = raw_input()
+y = raw_input()
+def func(x,y):
+   print(x+" says hello to "+y)
+
+func(x,y)
